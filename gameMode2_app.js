f00942ddf36cd4779ca834c4d710a08aab81cc7e
Danny040 <dann0051@gmail.com>
2023-09-30 16:46:49 +0300

    now 2 human players can play

--- gameMode2_app.js
+++ gameMode2_app.js
@@ -1,3 +1,89 @@
+const gameMoves = ["", "", "", "", "", "", "", "", ""];
+const winningCombinations = ["012", "345", "678", "036", "258", "048", "246", "147"];
+const turnTxt = document.getElementById("turn");
+let turn = "x";
+const gameSquares = document.querySelectorAll(".game-square");
+let x = 'Player 1';
+let o = 'Player 2';
+const gameResults = [0, 0, 0]; // x, tie, o
+
 export function humanGame(playersMarks) {
-    console.log(playersMarks.get("playerOne"));
+    console.log("inside human game: ");
+    if(playersMarks.get("playerOne") == 'o') {
+        document.getElementById("player1").innerHTML = "(Player 2)";
+        document.getElementById("player2").innerHTML = "(Player 1)";
+        x = 'Player 2';
+        o = 'Player 1';
+    }
+
+    
+    gameSquares.forEach((square) => {
+        square.addEventListener("click",() =>{
+            updateSquare(square, square.value);
+        });
+    });
+}
+
+function updateSquare(square, value) {
+    if (gameMoves[value] !== "") {
+        alert("This is taken.");
+        return;
+    }
+    gameMoves[value] = turn;
+    square.innerHTML = turn;
+    square.classList.add(`${turn}-move`);
+    if ( turn === "x") {
+        turn = "o";
+    }
+    else {
+        turn = "x";
+    }
+    turnTxt.innerHTML = turn;
+    checkGameOutcome();
+}
+
+function checkGameOutcome() {
+    for(let combination of winningCombinations) {
+        if(gameMoves[combination[0]] === gameMoves[combination[1]] && gameMoves[combination[0]] === gameMoves[combination[2]] && gameMoves[combination[0]] != "") {
+            if(gameMoves[Number(combination[0])] === "x") {
+                showWhoWon(1, combination); // x won
+                return;
+            }
+            showWhoWon(2, combination); // o won
+            return;
+        }
+        
+    }
+}
+
+function showWhoWon(result, squaresNumber) {
+    let numberOfButtons = gameSquares.length;
+    if(result === 1) {
+        for (let i = 0; i < numberOfButtons; i++) {
+            if(squaresNumber.includes(gameSquares[i].value)) {
+                gameSquares[i].classList.add("winner-x");
+            }
+        }
+        gameResults[0] += 1;
+        document.getElementById("popup-window").style.display = "flex";
+        document.getElementById("winner-mark").innerHTML = 'X';
+        document.getElementById("the-winner").innerHTML = x;
+        document.getElementById("won-number").innerHTML = gameResults[0];
+        // document.getElementById("next-round").addEventListener("click",() => {
+        //     location.reload();
+        //     start();
+        // });
+    }
+    else if (result === 2) {
+        for (let i = 0; i < numberOfButtons; i++) {
+            if(squaresNumber.includes(gameSquares[i].value)) {
+                gameSquares[i].classList.add("winner-o");
+            }
+        }
+        gameResults[2] += 1;
+        document.getElementById("popup-window").style.display = "flex";
+        document.getElementById("winner-mark").innerHTML = 'O';
+        document.getElementById("the-winner").innerHTML = o;
+        document.getElementById("lost-number").innerHTML = gameResults[2];
+    }
 }
--- page1_app.js
+++ page1_app.js
@@ -1,15 +1,11 @@
 import { cpuGame } from "./gameMode1_app.js";
 import { humanGame } from "./gameMode2_app.js";
+
 // below data is exported and sets some game properties
 const playersMarks = new Map([
     ["playerOne", "x"],
     ["playerTwo", "o"]
 ]);
-
-// const gameMode = new Map([
-//     ["vsCPU", 0],
-//     ["vsHuman", 0]
-// ]);
 
 //below code is for choosing first players mark
 const xChoice = document.getElementById("x-player");
@@ -40,17 +36,18 @@ const firstPage = document.getElementById("first-page");
 const secondPage = document.getElementById("second-page");
 
 cpuGameMode.addEventListener("click", () => {
-    // gameMode.set("vsCPU", 1);
     firstPage.style.display = "none";
     secondPage.style.display = "flex";
     cpuGame(playersMarks);
 });
 
 humanGameMode.addEventListener("click", () => {
-    // gameMode.set("vsHuman", 1);
     firstPage.style.display = "none";
     secondPage.style.display = "flex";
     humanGame(playersMarks);
 });
 
-// export {playersMarks, gameMode};
+function startHumanMode() {
+    
+}
+
